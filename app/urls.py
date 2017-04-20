17e27d7a66af922f21d6307edb78dc63a7d13baf
player <ammarinjtk@outlook.com>
2017-04-21 01:10:02 +0700

    there is no "main" application, create "app" application instead.
    create Person model (in local Mysql), then make profile.html to be able to get queryset from views.validate method.

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from main import views
+from app import views
 
 urlpatterns = [
-    url(r'^$', views.login, name = 'login'),
-    url(r'/teacher', views.login, name = 'teacher'),
-    url(r'/head', views.login, name = ''),
-    url(r'/officer', views.login, name = 'login'),
+    url(r'^$', views.index, name = 'login'),
+    url(r'^validate$', views.validate, name = 'validate'),
+    # url(r'/teacher', views.login, name = 'teacher'),
+    # url(r'/head', views.login, name = ''),
+    # url(r'/officer', views.login, name = 'login'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,28 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Person
+from . import views
+from django.http import JsonResponse
 
 # Create your views here.
+def index(request):
+    return render(request, "profile.html")
+
+def validate(request):
+    first_name = request.GET.get('first_name', None)
+
+    query = 'SELECT * FROM sql_person'
+    all_persons = Person.objects.raw(query)
+    selected_columns = ['first_name', 'last_name']
+    data = {}
+    for column in selected_columns:
+        data[column] = []
+
+    for person in all_persons:
+        for column in selected_columns:
+            if column == 'first_name':
+                data[column].append(person.first_name)
+            elif column == 'last_name':
+                data[column].append(person.last_name)
+
+    return JsonResponse(data)
